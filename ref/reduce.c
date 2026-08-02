3edd5af5991927164edd4aacebfcbee00b8064e7
Peter Schwabe <peter@cryptojedi.org>
2026-08-02 10:40:20 +0200

    Fix suggested by Philipp Klaus Krause (issue #109) for systems with integers that have less than 32 bits

--- ref/reduce.c
+++ ref/reduce.c
@@ -34,9 +34,9 @@ int16_t montgomery_reduce(int32_t a)
 **************************************************/
 int16_t barrett_reduce(int16_t a) {
   int16_t t;
-  const int16_t v = ((1<<26) + KYBER_Q/2)/KYBER_Q;
+  const int16_t v = ((1l<<26) + KYBER_Q/2)/KYBER_Q;
 
-  t  = ((int32_t)v*a + (1<<25)) >> 26;
+  t  = ((int32_t)v*a + (1l<<25)) >> 26;
   t *= KYBER_Q;
   return a - t;
 }
